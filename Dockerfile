7e9039dac3aefb6a99aadc5a885bd189dd95d7cc
kunalKrGcloud <kunal@gcloud.com>
2018-08-08 09:25:21 +0000

    cloud commit on docker1

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
+VOLUME log
 ARG JAR_FILE
 COPY target/demo1-2.0.1.RELEASE.war app.jar
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
--- src/main/java/com/airtel/demo1/ApplicationController.java
+++ src/main/java/com/airtel/demo1/ApplicationController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.UUID;
 
 @RestController
 public class ApplicationController {
@@ -14,8 +17,20 @@ public class ApplicationController {
     @RequestMapping(name = "/index",method = RequestMethod.GET)
     public String index() throws Exception{
 
-        System.out.println("Index Action called in from DOCKER 1 controller at :"+(new Date()).toString());
+  Logger logger = LoggerFactory.getLogger(ApplicationController.class);
+       UUID uuid = UUID.randomUUID();
+        String randomUUIDString = uuid.toString();
+
+         for(int i=0;i<10;i++){
+	 
+	        logger.info("This is an information message {} {}",randomUUIDString,i);
+
+	        logger.error("this is a error message {} {}",randomUUIDString,i);
+
+	        logger.warn("this is a warning message {} {}",randomUUIDString,i);
+             }
+        System.out.println("Index Action called in from DOCKER 1 controller at :"+randomUUIDString+(new Date()).toString());
 //        demoService.springReTryTest();
-        return "Index called From Application DOCKER 1";
+        return "Index called From Application DOCKER 1"+randomUUIDString;
     }
 }
